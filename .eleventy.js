87dd4be1afa0e5de82e6fb5f41506107f7057fb1
Laurens Groeneveld <me@laurens.io>
2022-01-11 11:33:43 +0100

    Move layouts to separate folder

--- .eleventy.js
+++ .eleventy.js
@@ -19,6 +19,6 @@ module.exports = function (eleventyConfig) {
     });
 
     return {
-        dir: { input: 'site', output: 'dist', includes: '_includes' },
+        dir: { input: 'site', output: 'dist', includes: '_includes', layouts: '_includes/layouts' },
       };
 };
